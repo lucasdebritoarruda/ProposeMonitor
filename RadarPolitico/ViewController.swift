03df860bf2492700ba57876f37d8f78e0913a726
Lucas De Brito <lucasdebritoarruda@gmail.com>
2018-01-07 14:40:48 -0300

    Initial Commit

---
+++ RadarPolitico/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RadarPolitico
+//
+//  Created by Lucas de Brito on 07/01/2018.
+//  Copyright © 2018 Lucas de Brito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
